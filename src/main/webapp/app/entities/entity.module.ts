3253bcc375cc7f8cad851da0203caa0d273b7a40
Ousmane Sow <nureynisow@gmail.com>
2019-02-19 20:15:58 +0000

    use jh to add new models

--- src/main/webapp/app/entities/entity.module.ts
+++ src/main/webapp/app/entities/entity.module.ts
@@ -4,6 +4,18 @@ import { RouterModule } from '@angular/router';
 @NgModule({
     imports: [
         RouterModule.forChild([
+            {
+                path: 'agence',
+                loadChildren: './agence/agence.module#YoonneeAgenceModule'
+            },
+            {
+                path: 'client',
+                loadChildren: './client/client.module#YoonneeClientModule'
+            },
+            {
+                path: 'transaction',
+                loadChildren: './transaction/transaction.module#YoonneeTransactionModule'
+            }
             /* jhipster-needle-add-entity-route - JHipster will add entity modules routes here */
         ])
     ],
