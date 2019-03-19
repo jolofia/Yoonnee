256537e96fa5131ec6a92c6faf399eea0f5497b3
Ousmane Sow <nureynisow@gmail.com>
2019-03-19 21:09:32 +0000

    create afence done

--- src/main/webapp/app/shared/model/agence.model.ts
+++ src/main/webapp/app/shared/model/agence.model.ts
@@ -6,3 +6,23 @@ export interface IAgence {
 export class Agence implements IAgence {
     constructor(public id?: string, public nom?: string) {}
 }
+
+// export interface IAgence {
+//     id?: string;
+//     nom: string;
+//     adresse: string;
+//     ville: string;
+//     pays: string;
+//     telephone: string;
+//     gerant: string[];
+// }
+//
+// export class Agence implements IAgence {
+//     adresse: string;
+//     gerant: string[];
+//     id: string;
+//     nom: string;
+//     pays: string;
+//     telephone: string;
+//     ville: string;
+// }
